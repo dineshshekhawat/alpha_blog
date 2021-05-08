002900c333a16b550983eb28405ec2c9699343ae
Dinesh <shekhawat.dinesh@gmail.com>
2021-05-08 19:27:32 +0530

    Create show action and add gravitar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if 
+        if @user.save
             flash[:success] = "Welcome to the alpha blog #{@user.username}"
             redirect_to articles_path
         else
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
     
     def edit
         @user = User.find(params[:id])
-
     end
     
     def update
@@ -26,6 +25,10 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end
+    end
+
+    def show
+        @user = User.find(params[:id])
     end
 
     private
